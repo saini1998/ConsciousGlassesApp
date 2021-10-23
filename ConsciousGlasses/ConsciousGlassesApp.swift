09df41fed19613327d685a654607bc4a3240abc8
saini1998 <sainiaaryaman1998@gmail.com>
2021-10-23 03:32:44 +0100

    Initial Commit

---
+++ ConsciousGlasses/ConsciousGlassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsciousGlassesApp.swift
+//  ConsciousGlasses
+//
+//  Created by Aaryaman Saini on 23/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ConsciousGlassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConsciousGlasses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConsciousGlasses
+//
+//  Created by Aaryaman Saini on 23/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
